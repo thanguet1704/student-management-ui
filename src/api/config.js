75569f6e6c86caf621faef0ea9ca089667b03ecc
thanguet1704 <ngocthanhotboy@gmail.com>
2021-09-26 05:00:28 +0700

    make it better

--- src/api/config.js
+++ src/api/config.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 export const axiosClient = axios.create({
-  baseURL: `https://36d131966c22.ngrok.io`,
+  baseURL: `https://hcma.herokuapp.com/`,
   headers: {
     'content-type': 'application/json',
     Authorization: `Bearer ${Cookies.get('hcmaid')}`,
